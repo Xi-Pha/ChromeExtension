08b5366adfc6256550a765829d47a0b75d6a3af9
MJFalahi <mjfalahi@outlook.com>
2022-08-11 03:06:29 +0430

    Next commit

--- logic/popup.js
+++ logic/popup.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let apply_button = document.getElementById('apply_btn');
     let database_button = document.getElementById('database_btn');
+    let factor_button = document.getElementById('factor');
+    let define_button = document.getElementById('define');
+
     var server_ip;
     var server_port;
     var domain;
@@ -51,7 +54,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     database_button.addEventListener("click", async () => {
-        window.location = "database.html";
+        window.open('database.html','_blank');
+    });
+
+    factor_button.addEventListener("click", async () => {
+        window.open('factor.html','_blank');
+    });
+
+    define_button.addEventListener("click", async () => {
+        window.open('define.html','_blank');
     });
 
     function apply_variables() {
